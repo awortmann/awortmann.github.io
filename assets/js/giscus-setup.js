cc842f847d832ea9fca73a02b370329d28d03f4f
awortmann <andreas.wortmann@isw.uni-stuttgart.de>
2026-05-17 07:25:59 +0000

    Deploying to gh-pages from @ awortmann/awortmann.github.io@3469d504a82b185818c1871c06337d6762735c0b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "",
+    "data-repo": "awortmann/awortmann.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
